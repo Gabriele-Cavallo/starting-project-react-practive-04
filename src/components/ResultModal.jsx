24d4b76d0ffd38f15eba6d18b3be0da73446452c
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-08-06 19:02:46 +0200

    feat: add winning logic

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,6 +1,11 @@
 import { forwardRef, useImperativeHandle, useRef } from "react";
-const ResultModal = forwardRef(function ResultModal({ result, targetTime }, ref) {
+const ResultModal = forwardRef(function ResultModal({ onReset, remaininTime, targetTime }, ref) {
     const dialog = useRef();
+
+    const userLost = remaininTime <= 0;
+    const formattedRemainingTime = (remaininTime / 1000).toFixed(2);
+    const score = Math.round((1 - remaininTime / (targetTime * 1000)) * 100);
+
     useImperativeHandle(ref, () => {
         return {
             open() {
@@ -10,11 +15,12 @@ const ResultModal = forwardRef(function ResultModal({ result, targetTime }, ref)
     });
 
     return(
-        <dialog ref={dialog} className="result-modal">
-            <h2>You {result}</h2>
+        <dialog ref={dialog} onClose={onReset} className="result-modal">
+            {userLost && <h2>You Lost</h2>}
+            {!userLost && <h2>Your Score: {score}</h2>}
             <p>The target time was <strong>{targetTime}</strong> seconds.</p>
-            <p>You stopped the timer with <strong>X seconds left.</strong></p>
-            <form method="dialog" action="">
+            <p>You stopped the timer with <strong>{formattedRemainingTime} seconds left.</strong></p>
+            <form onSubmit={onReset} method="dialog" action="">
                 <button>Close</button>
             </form>
         </dialog>
