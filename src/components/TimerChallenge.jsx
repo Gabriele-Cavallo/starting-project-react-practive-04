f855bf872bd8962e0aa3a7572955611b13e6e78c
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-08-06 18:11:16 +0200

    feat: add ResultModal component with forwardRef function

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,7 +1,8 @@
 import { useRef, useState } from "react";
-
+import ResultModal from "./ResultModal";
 export default function TimerChallenge({ title, targetTime }){
     const timer = useRef();
+    const dialog = useRef();
 
     const [ timerStarted, setTimerStarted ] = useState(false);
     const [ timerExpired, setTimerExpired ] = useState(false);
@@ -11,6 +12,7 @@ export default function TimerChallenge({ title, targetTime }){
         setTimerStarted(true);
         timer.current = setTimeout(() => {
             setTimerExpired(true);
+            dialog.current.showModal();
         }, targetTime * 1000);
     }
     function handleStop(){
@@ -18,9 +20,9 @@ export default function TimerChallenge({ title, targetTime }){
     }
     return (
         <>
+            <ResultModal ref={dialog} result="lost" targetTime={targetTime} />
             <section className="challenge">
                 <h2>{title}</h2>
-                {timerExpired && <p>You lost!</p>}
                 <p className="challenge-time">{targetTime} second{targetTime > 1 ? 's' : ''}</p>
                 <p>
                     <button onClick={timerStarted ? handleStop : handleStart}>
